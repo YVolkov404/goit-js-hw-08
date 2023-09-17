7c518cdab7dc86a7a28495b2f1aecac88e2c5654
Yuriy Volkov <sir.volkov@outlook.com>
2023-09-17 20:39:14 +0300

    upd : task-02 cmplt

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,4 @@
+import { player, currentTime } from './player-setup';
 
+// getting a current time playback ov the video from a local storage after the site page was reloaded
+player.setCurrentTime(currentTime);
